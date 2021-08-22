af1392342f2b0656eb5b5d9022607df45b39dd48
mathiasburgos <mathias_burgos@icloud.com>
2021-08-22 20:37:47 -0300

    commit on 'Master'

--- js/login.js
+++ js/login.js
@@ -1,6 +1,27 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
-});
+document.addEventListener("DOMContentLoaded", function (e) {
+    document.getElementById("logbtn").addEventListener("click", function() {
+      let inputEmail = document.getElementById("usuario");
+      let inputPassword = document.getElementById("password");
+      let camposCompletos = true;
+      if (inputEmail.value === '') {
+        inputEmail.classList.add("invalid");
+        camposCompletos = false;
+      } else {
+        inputEmail.classList.remove("invalid");
+      }
+      if (inputPassword.value === '') {
+        inputPassword.classList.add("invalid");
+        camposCompletos = false;
+      } else {
+        inputPassword.classList.remove("invalid");
+      }
+      if (camposCompletos) {
+        window.location = "main.html";
+      } else {
+        alert("Ingresar los datos por favor");
+      }
+    })
+  });
